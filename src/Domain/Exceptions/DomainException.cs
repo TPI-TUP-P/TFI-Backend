9a7e16723307f97f71dbcc6b1ea1b58c2894153c
Tomas Dev <152812185+TomasB-Dev@users.noreply.github.com>
2026-07-17 11:03:08 -0300

    fix error in domainexception

--- src/Domain/Exceptions/DomainException.cs
+++ src/Domain/Exceptions/DomainException.cs
@@ -2,4 +2,15 @@ namespace Domain.Exceptions;
 
 public abstract class DomainException : Exception
 {
+    protected DomainException(string message, int statusCode) : base(message)
+    {
+        StatusCode = statusCode;
+    }
+
+    public int StatusCode { get; }
+
+    protected DomainException(string message) : base(message)
+    {
+        StatusCode = 400;
+    }
 }
---
+++ src/Infrastructure/Migrations/20260717140047_addlogicdelete.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addlogicdelete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "State",
+                table: "Publications",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "Postulations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    UserId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    JobOfferId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    State = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Postulations", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Postulations");
+
+            migrationBuilder.DropColumn(
+                name: "State",
+                table: "Publications");
+        }
+    }
+}
