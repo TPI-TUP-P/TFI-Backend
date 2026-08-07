4f7c9320c30b6cd4ee308f5e5b853dbf75260003
Tomas Dev <152812185+TomasB-Dev@users.noreply.github.com>
2026-08-07 15:58:09 -0300

    add publication existis

--- backend/src/Domain/Interfaces/IPublicationRepository.cs
+++ backend/src/Domain/Interfaces/IPublicationRepository.cs
@@ -5,6 +5,5 @@ namespace Domain.Interfaces;
 
 public interface IPublicationRepository : IGenericRepository<Publication>
 {
-
-
+    Task<bool> ExistsAsync(Guid id, CancellationToken cancellationToken);
 }
